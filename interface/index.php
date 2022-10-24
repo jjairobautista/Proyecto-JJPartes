b7637bc5d508e1fd7f2299db7b6a7eda6b210dca
jjairobautista <jhonbautista980@gmail.com>
2022-10-24 13:35:42 -0500

    first commit

---
+++ interface/index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/stilos.css">
+    <title>JJ Parte Inicio</title>
+</head>
+
+<body>
+    <div class="container ">
+
+        <div class="text center">
+            <nav class="navbar navbar-expand-lg bg-light p-2">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled">Disabled</a>
+                            </li>
+                        </ul>
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div class=" text-center">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="../img/16_Blog_DA.png" width="500px" height="500px" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../img/amortiguadores-1.jpg" width="50%" height="500px" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../img/amortiguadores-auto.jpg" width="50%" height="500px" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+        </div>
+        <br>
+        <div class="p-2" id="fondoQuienesSomos">
+            <br><br><br><br>
+
+            <div class=" text-center text-dark p-2" id="quienesSomos">
+                <h2>¿Quienes Somos?</h2>
+                <p>Somos un grupo de asesores comerciales especializados en la suspencion,</p>
+                <p> direcciones y amortiguadrores automotriz que te guiaremos con la compra de </p>
+                <p> los repuestos para tu carro, no te lo pierdas.</p>
+            </div>
+        </div>
+        <br>
+        <div class="row text-center">
+            <br><br>
+            <h2>Trabajamos Con Las Siguientes Marcas</h2>
+            <br><br>
+            <br><br>
+
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/logos/nissan.jpg" width="200px" height="200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">NISAN</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/logos/mazda.jpeg" width="200px" height="200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">MAZDA</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/logos/Renault.png" width="200px" height="200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">RENAULT</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/logos/porsche.jpg" width="200px" height="200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">PORSCHE</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row text-center">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/logos/peugeot.png" width="200px" height="200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">PEUGEOT</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/logos/mercedes.jpg" width="200px" height="200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">MERCEDES-BENZ</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/logos/logoChevrolet.jfif" width="200px" height="200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">CHEVROLT</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/logos/jaguar.png" width="200px" height="200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">JAGUAR</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row text-center">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/logos/bmw.jpg" width="200px" height="200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">BMW</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/logos/citroen.png" width="200px" height="200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">CITROEN</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/logos/ford.png" width="200px" height="200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">FORD</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="../img/logos/honda.png" width="200px" height="200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">HONDA</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br><br><br><br><br><br><br><br><br><br>
+
+        <footer>
+            <div class="row  bg-dark">
+                <div class="col">
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label text-light">Nombre Completo</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Jhon Jairo Bautista">
+                        <label for="exampleFormControlInput1" class="form-label text-light">Correo</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="jhonbautista9800@gmail.com">
+                        <label for="exampleFormControlInput1" class="form-label text-light">Telefono</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="3114640388">
+                    </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlTextarea1" class="form-label text-light">Descripcion</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </div>
+                <div class="col"><br><br><br><img src="../img/logoJJpartes.png" width="400px" height="200px" class="rounded float-end" alt=""></div>
+            </div>
+        </footer>
+        <br>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
